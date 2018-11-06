6e23ddbf3d3054d0d34db0dee4816278ff7cb6ba
Alex Krolick <akrolick@box.com>
2018-11-06 15:36:35 -0800

    Chore: set up jest test env for react-testing-library

--- build/jest-setup.js
+++ build/jest-setup.js
@@ -3,5 +3,11 @@ import 'core-js/es6/set';
 import 'raf/polyfill';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-dom/extend-expect';
+import { cleanup } from 'react-testing-library';
+
+afterEach(() => {
+    cleanup();
+});
 
 Enzyme.configure({ adapter: new Adapter() });
