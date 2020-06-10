0791fd677c0b3ae783c5abb4bd083c0d5669ccb9
Alex Krolick <akrolick@box.com>
2020-06-10 11:23:25 -0700

    feat(usm): active/inactive table & revokation

--- src/features/unified-share-modal/ManageSharedLinkModal.js
+++ src/features/unified-share-modal/ManageSharedLinkModal.js
@@ -5,6 +5,7 @@ import { FormattedMessage } from 'react-intl';
 import { Modal, ModalActions } from '../../components/modal';
 import Button from '../../components/button';
 import PlainButton from '../../components/plain-button';
+import PrimaryButton from '../../components/primary-button';
 import ReadableTime from '../../components/time/ReadableTime';
 import LoadingIndicator from '../../components/loading-indicator';
 import IconCopy from '../../icons/general/IconCopy';
@@ -60,10 +61,12 @@ function ManageSharedLinkModal(props: Props) {
                                 itemId: "${item.id}",
                                 itemType: "${item.type}"
                             ) {
+                                id
                                 shareName
                                 createdAt
                                 expiration
                                 maxUses
+                                revoked
                                 item {
                                     name
                                 }
@@ -86,9 +89,67 @@ function ManageSharedLinkModal(props: Props) {
         dispatch({ type: 'links/success', payload: json.data });
     };
 
+    const revokeLink = async id => {
+        let json;
+        dispatch({ type: 'linkRevoke/start', payload: {} });
+        try {
+            const res = await fetch('/app-api/graphql', {
+                headers: {
+                    accept: '*/*',
+                    'accept-language': 'en-US,en;q=0.9',
+                    'content-type': 'application/json',
+                },
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify({
+                    operationName: null,
+                    variables: {},
+                    query: `
+                        mutation {
+                            revokeShareLink(id: "${id}") {
+                                shareName
+                                id
+                            }
+                        }
+                    `,
+                }),
+                method: 'POST',
+                mode: 'cors',
+                credentials: 'include',
+            });
+            json = await res.json();
+        } catch (error) {
+            dispatch({ type: 'linkRevoke/fail', payload: { error } });
+        }
+        dispatch({ type: 'linkRevoke/success', payload: json.data });
+    };
+
     React.useEffect(() => {
         fetchLinks();
     }, []); // eslint-disable-line
+
+    let sortedLinks = { activeLinks: [], inactiveLinks: [] };
+
+    if (!state.isLoading && state.links) {
+        sortedLinks = state.links
+            .map(link => {
+                const usesCount = Math.ceil(Math.random() * (link.maxUses || 1));
+                return { ...link, usesCount };
+            })
+            .reduce(
+                ({ inactiveLinks, activeLinks }, link) => {
+                    const now = new Date();
+                    const inactive =
+                        ((link.maxUses || 0) >= 1 && link.usesCount >= link.maxUses) ||
+                        (link.expiration && new Date(link.expiration) < now) ||
+                        !!link.revoked;
+                    return {
+                        inactiveLinks: [...inactiveLinks, inactive ? link : undefined].filter(Boolean),
+                        activeLinks: [...activeLinks, !inactive ? link : undefined].filter(Boolean),
+                    };
+                },
+                { inactiveLinks: [], activeLinks: [] },
+            );
+    }
 
     return (
         <Modal
@@ -105,21 +166,21 @@ function ManageSharedLinkModal(props: Props) {
             className="bdl-ManageSharedLinkModal"
             {...modalProps}
         >
-            <table className="bdl-Table table">
+            <h5>Active Links</h5>
+            <table className="bdl-Table table mbl">
                 <thead>
                     <tr>
                         <th style={{ width: '150px' }}>Created By</th>
-                        <th>Shared Link ID</th>
+                        <th style={{ minWidth: '150px' }}>Shared Link ID</th>
                         <th style={{ width: '70px' }} />
-                        <th>Expiration</th>
-                        <th style={{ width: '1%' }}>Uses</th>
-                        <th style={{ width: '1%' }} />
+                        <th style={{ width: '150px' }}>Expiration</th>
+                        <th style={{ width: '80px' }}>Uses</th>
+                        <th style={{ width: '70px' }} />
                     </tr>
                 </thead>
                 <tbody>
                     {state.links &&
-                        state.links.map(link => {
-                            const usesCount = Math.ceil(Math.random() * (link.maxUses || 1));
+                        sortedLinks.activeLinks.map((link: any) => {
                             return (
                                 <tr className="shared-link-row" key={link.id}>
                                     <td>
@@ -146,20 +207,25 @@ function ManageSharedLinkModal(props: Props) {
                                         <small>People in your company can view</small>
                                     </td>
                                     <td>
-                                        <PlainButton>
+                                        <PlainButton className="bdl-sharedLinkTable-copy">
                                             <IconCopy />
                                         </PlainButton>
                                     </td>
-                                    <td>
+                                    <td style={{ minWidth: 100 }}>
                                         {link.expiration ? (
                                             <ReadableTime timestamp={new Date(link.expiration).getTime()} />
                                         ) : (
                                             'Never'
                                         )}
                                     </td>
-                                    <td>{link.maxUses ? `${usesCount}/${link.maxUses}` : `${usesCount}`}</td>
+                                    <td>{link.maxUses ? `${link.usesCount}/${link.maxUses}` : `${link.usesCount}`}</td>
                                     <td style={{ textAlign: 'right' }}>
-                                        <PlainButton className="bdl-sharedLink-revoke">
+                                        <PlainButton
+                                            className="bdl-sharedLink-revoke"
+                                            onClick={() => {
+                                                revokeLink(link.id);
+                                            }}
+                                        >
                                             <span style={{ color: vars.bdlWatermelonRed }}>Revoke</span>
                                         </PlainButton>
                                     </td>
@@ -173,7 +239,80 @@ function ManageSharedLinkModal(props: Props) {
                     <LoadingIndicator />
                 </p>
             ) : null}
-            <ModalActions>
+            {!state.isLoading && (
+                <>
+                    <h5 className="mtl">Inactive or Expired Links</h5>
+                    <table className="bdl-Table table">
+                        <thead>
+                            <tr>
+                                <th style={{ width: '150px' }}>Created By</th>
+                                <th style={{ minWidth: '150px' }}>Shared Link ID</th>
+                                <th style={{ width: '70px' }} />
+                                <th style={{ width: '150px' }}>Expiration</th>
+                                <th style={{ width: '80px' }}>Uses</th>
+                                <th style={{ width: '70px' }} />
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {state.links &&
+                                sortedLinks.inactiveLinks.map(link => {
+                                    return (
+                                        <tr className="shared-link-row" key={link.id} style={{ opacity: '67%' }}>
+                                            <td>
+                                                {link.createdBy.publicName}
+                                                <br />
+                                                <small>
+                                                    <ReadableTime timestamp={new Date(link.createdAt).getTime()} />
+                                                </small>
+                                            </td>
+                                            <td>
+                                                <span
+                                                    style={{
+                                                        fontFamily: 'monospace',
+                                                        maxWidth: 80,
+                                                        display: 'inline-block',
+                                                        overflowX: 'hidden',
+                                                        textOverflow: 'ellipsis',
+                                                    }}
+                                                    title={link.shareName}
+                                                >
+                                                    {link.shareName}
+                                                </span>
+                                                <br />
+                                                <small>People in your company can view</small>
+                                            </td>
+                                            <td>
+                                                <PlainButton className="bdl-sharedLinkTable-copy">
+                                                    <IconCopy />
+                                                </PlainButton>
+                                            </td>
+                                            <td style={{ minWidth: 100 }}>
+                                                {link.expiration ? (
+                                                    <ReadableTime timestamp={new Date(link.expiration).getTime()} />
+                                                ) : (
+                                                    'Never'
+                                                )}
+                                            </td>
+                                            <td>
+                                                {link.maxUses
+                                                    ? `${link.usesCount}/${link.maxUses}`
+                                                    : `${link.usesCount}`}
+                                            </td>
+                                            <td style={{ textAlign: 'right' }}>
+                                                <PlainButton className="">
+                                                    {!!link.revoked && (
+                                                        <span style={{ color: vars.bdlWatermelonRed }}>Revoked</span>
+                                                    )}
+                                                </PlainButton>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                        </tbody>
+                    </table>
+                </>
+            )}
+            <ModalActions style={{ justifyContent: 'space-between' }}>
                 <Button onClick={onRequestClose}>
                     <FormattedMessage
                         id="be.unifiedshare.sharedlink.new"
@@ -181,13 +320,13 @@ function ManageSharedLinkModal(props: Props) {
                         description="shared link management - creation button"
                     />
                 </Button>
-                <Button onClick={onRequestClose} {...cancelButtonProps}>
+                <PrimaryButton onClick={onRequestClose} {...cancelButtonProps}>
                     <FormattedMessage
                         id="be.unifiedshare.sharedlink.done"
                         defaultMessage="Done"
                         description="shared link management modal title"
                     />
-                </Button>
+                </PrimaryButton>
             </ModalActions>
         </Modal>
     );
